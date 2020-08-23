049b8d4ec4101ba819186925f9e6c470d2e90379
nandhini-47 <nandhinij@juhomi.com>
2020-08-23 16:13:39 +0530

    product commit

--- db/migrate/20200810111158_create_products.rb
+++ db/migrate/20200810111158_create_products.rb
@@ -5,8 +5,24 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.text :description
       t.string :image_url
       t.decimal :price, precision: 8, scale: 2
-
       t.timestamps
     end
   end
 end
+
+# browser
+# 
+# user can disable in browser 
+
+# advan
+# validation is common for andrid,ios
+# disadvan
+# requests the network which takes time
+
+# Advan
+# rails validation are data base agnostic it will for all data bases like pg, mysql
+
+
+# disadvan
+# if database access is accesed by more than 1 application - have validation in database
+# for trigger/store procedure will have dif lang
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  test "Product attribute can not be empty" do
+    product = Product.new
+    assert product.invalid?
+    puts product.errors.inspect
+    puts product.errors[:title]
+    assert product.errors[:title].any?
+    assert product.errors[:price].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "negative price validation" do
+  	product = Product.new
+  	product.price = -1
+  	assert product.invalid?
+  	assert product.errors[:price].any?
+  	puts product.errors[:price]
+  	assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+  end
+
+  test "positive price validation" do
+  	product = Product.new(title: "Validation Test1", description: "price validation", image_url: "zzz.jpg")
+  	product.price = 1 
+  	assert product.valid?
+  	# puts product.errors.to_json
+  	
+  end
+
+  test "product validation" do
+  	product = Product.new(title: "Validation Test", description: "price validation", image_url: "zzz.jpg", price: 1)
+  	assert product.invalid?
+  	puts product.errors[:title]
+  	assert product.errors[:title].any?
+  	assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  # test "product title must be unique" do
+  # 	product = Product.new(title: "Validation Test", description: "price validation", image_url: "zzz.jpg", price: 1)
+  # 	product.save
+  # 	assert product.valid?
+  # end
+
+  # test "product title must be unique 1" do
+  # 	product = Product.new(title: "Validation Test", description: "price validation", image_url: "zzz.jpg", price: 1)
+  # 	product.save
+  # 	assert product.valid?
+  # end
+
+  # test "product title must be unique 2" do
+  # 	product = Product.new(title: "Validation Test", description: "price validation", image_url: "zzz.jpg", price: 1)
+  # 	assert product.invalid?
+  # 	assert.product.errors[:title].any?
   # end
 end
